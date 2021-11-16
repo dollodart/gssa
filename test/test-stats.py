d16d2b72ce40e9296bfeb9a99bc3bee4c0e1c854
david <davidollodart@gmail.com>
2021-11-16 11:09:18 -0800

    test-stats now returns 10 greatest results

--- test/test-stats.py
+++ test/test-stats.py
@@ -5,11 +5,11 @@ publist = load_data()
 
 tcites = citing_authors(publist)
 d = value_counts(tcites)
-print(sorted([(x, f'{d[x] / len(tcites):.2f}') for x in d], key=lambda x:x[1]))
+print(sorted([(x, f'{d[x] / len(tcites):.2f}') for x in d], key=lambda x:x[1])[-10:])
 
 ncites = ncitations(publist)
 d = groupby(ncites)
-print(sorted([(x, sum(d[x])) for x in d], key=lambda x:x[1]))
+print(sorted([(x, sum(d[x])) for x in d], key=lambda x:x[1])[-10:])
 
 from serp.stats.metrics import hindex, gindex, i10index, oindex, windex
 
