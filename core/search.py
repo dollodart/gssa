8267822cbba50e36f5ff9fdf6809f546e2f7e5d2
david <davidollodart@gmail.com>
2021-11-16 11:25:22 -0800

    added caching for search
    as noted in cf48d0, the search caching is lossy because it only
    caches the organic results, not the search metadata and other fields.
    that should be fixed

--- core/search.py
+++ core/search.py
@@ -1,20 +1,28 @@
-import requests
-from serp.env import CACHE_DIR, URL
-from serp.env import query_dictionary, pagination_dictionary
-from serp.env import core_logger
-from serp.env import global_checker
 from time import time
-from serp.query import json_request, flatten_pagination
+from serp.env import query_dictionary, core_logger, global_checker
+from serp.query import json_request, flatten_pagination, cache, load_cache
 from .publication import Publication
 
-def search(query_term):
+def hash_dict(dct):
+    slist = sorted(key + dct[key] for key  in dct)
+    return ''.join(slist)
+
+def search(query_term, overwrite=False):
     query_dictionary['q'] = query_term
-    core_logger.info(f'finding first page in query for term:{query_term}')
+    query_hash = hash_dict(query_dictionary)
+    res = load_cache(query_hash)
+
+    if res is not None and not overwrite:
+        core_logger.info(f'search results for query {query_term} cached, returning')
+        return res
+
+    core_logger.info(f'finding first page in search for term:{query_term}')
     dt, data = json_request(query_dictionary)
     core_logger.info(f'found in {dt}s')
     global_checker.increment()
-    core_logger.info(f'querying and flattening pagination')
+    core_logger.info(f'getting pages and flattening pagination')
     dt, data = flatten_pagination(data)
     core_logger.info(f'took {dt}s for {len(data)} results')
     global_checker.increment()
+    cache(data, query_hash)
     return [Publication.from_json(d) for d in data]
